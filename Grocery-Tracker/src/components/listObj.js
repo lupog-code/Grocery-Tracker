fad0ba3fa35b27815aebd7b263ec1b8123215792
lupog-code <gabri.lupo374@gmail.com>
2025-05-17 17:20:25 +0200

    Update listObj.js

--- Grocery-Tracker/src/components/listObj.js
+++ Grocery-Tracker/src/components/listObj.js
@@ -5,14 +5,9 @@ import {Ionicons} from "@expo/vector-icons";
 import {PopUp_editProduct} from "./modalObj";
 import { useNavigation } from '@react-navigation/native';
 import { buyItem } from '../data/db';
-const getDate = (timestamp) => {
-    const date = new Date(Number(timestamp));
 
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = String(date.getFullYear()).slice(-2); // Ultime 2 cifre
-
-    return `${day}/${month}/${year}`;
+const getDate = (data) => {
+    return data.split(' ')[0];
 };
 
 const getEmoji = ({ category }) => {
