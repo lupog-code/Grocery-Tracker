3315976548c14867595ad45f6854afcbd7adb994
lupog-code <gabri.lupo374@gmail.com>
2025-05-17 12:13:19 +0200

    Update HomeScreen.js

--- Grocery-Tracker/src/screens/HomeScreen.js
+++ Grocery-Tracker/src/screens/HomeScreen.js
@@ -23,7 +23,7 @@ const HomeScreen = () => {
         };
         fetchProducts();
     }, [])
-    
+
     useEffect(() => {
         const fetchLists = async () => {
             try {
@@ -41,7 +41,7 @@ const HomeScreen = () => {
         <View style={commStyle.body}>
 
             <View style={commStyle.flexView}>
-                <Text style={commStyle.homeTitle}>Home Page</Text>
+                <Text style={commStyle.homeTitle}>Lists</Text>
                 <TouchableOpacity>
                     <View style={commStyle.recentButton}>
                         <Ionicons name="time" style={commStyle.recentButtonIcon}/>
