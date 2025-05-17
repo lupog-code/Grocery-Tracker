514994b567bf1363cc01f8e2c4d36fb204261a05
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-05-17 12:06:19 +0200

    categorys page

---
+++ Grocery-Tracker/src/screens/CategoryScreen.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import {View, Text, Image, Touchable, TouchableOpacity, ScrollView, SafeAreaView, FlatList} from 'react-native';
+import commStyle from '../styles/commonStyle';
+import {Ionicons} from "@expo/vector-icons";
+import {List} from "../components/listObj";
+import {AddListBtn} from "../components/btnsObj";
+import { getListe } from '../data/db';
+
+const ListsScreen = () => {
+    const [lists, setLists] = useState([]);
+
+    useEffect(() => {
+            const fetchLists = async () => {
+                try {
+                    const data = await getListe();
+                    console.log("Fetched lists:", data);
+                    setLists(data);
+                } catch (error) {
+                    console.error("Error fetching lists:", error);
+                }
+            };
+            fetchLists();
+        }
+        , [])
+
+    return (
+        <View style={commStyle.body}>
+
+            <View style={commStyle.flexView2}>
+                <TouchableOpacity style={commStyle.sideBlock}>
+                    <Text style={commStyle.gobackText}>Home</Text>
+                </TouchableOpacity>
+
+                <View style={commStyle.titleBlock}>
+                    <Text style={commStyle.homeTitle2}>Categorys Page</Text>
+                </View>
+
+                <View style={commStyle.sideBlock} />
+            </View>
+
+
+        </View>
+    );
+}
+
+export default ListsScreen;
