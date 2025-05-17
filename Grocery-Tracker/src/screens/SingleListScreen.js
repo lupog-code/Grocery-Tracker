70faa657ead9396778132da6b0036116577845c7
lupog-code <gabri.lupo374@gmail.com>
2025-05-17 12:39:48 +0200

    Update SingleListScreen.js

--- Grocery-Tracker/src/screens/SingleListScreen.js
+++ Grocery-Tracker/src/screens/SingleListScreen.js
@@ -105,7 +105,7 @@ const ListsScreen = ({navigation , route}) => {
             renderItem={({ item }) => (
                 //Se l'elemento è stato comprato, mostra OldProduct, altrimenti mostra Product
                  item.comprato ?
-                <OldProduct onDelete={(id)=>handleDelete(id)} id={item.id} name={item.name} quantity={item.quantity} price={item.price} category={item.category} data={item.data_compera} />
+                <OldProduct onDelete={(id)=>handleDelete(id)} id={item.id} name={item.name} quantity={item.quantity} price={item.price} category={item.category} data={item.data_compera.split(' ')[0]} />
                 :
                 <Product onDelete={(id)=>handleDelete(id)} id={item.id} name={item.name} quantity={item.quantity} price={item.price} category={item.category} />
             )}
